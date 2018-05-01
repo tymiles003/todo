3c14c1f62114fa6968e4c4f28db12664d8d5a059
silksofthesoul <silksofthesoul@yandex.ru>
2018-05-02 02:39:49 +0300

    lib update / add new methods

--- src/components/lib/math.js
+++ src/components/lib/math.js
@@ -1,5 +1,55 @@
 export default (() => {
   let obj = {};
+
+  obj.rndMinMax = (min, max) => {
+      return Math.random() * (max - min) + min;
+  }; //		rndMinMax
+
+  obj.rndMinMaxInt = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+  }; //		rndMinMaxInt
+
+  obj.rndCoinInt = () => {
+      /**
+       * случайный 0 или 1
+       */
+      return ~~Math.floor(Math.random()*2);
+  };//rndCoinInt
+
+  obj.rndCoinBool = () => {
+      /**
+       * случайный true или false
+       */
+      return (~~Math.floor(Math.random()*2) === 0);
+  };//rndCoinBool
+
+  obj.rndCoinFlip = (a,b) => {
+      /**
+       * случайный a или b
+       */
+      return (~~Math.floor(Math.random()*2) === 0)?a:b;
+  };//rndCoinFlip
+
+  obj.uniqArrayInt = (length, min, max) => {
+      var unq = [];
+      var rnd = 0;
+      var flag = false;
+      if(max - min < length){
+          length = max - min;
+      }
+      while(unq.length < length){
+          rnd = obj.rndMinMaxInt(min,max);
+          flag = false;
+          for(var i =0;i<unq.length;i++){
+              if(unq[i]==rnd){flag=true;break;}
+          }
+          if(!flag){
+              unq.push(rnd);
+          }
+      }//while
+      return unq;
+  };
+
   obj.getPersentOfValues = (intValue, intMax) => {
     if (parseInt(intValue) === 0 || parseInt(intMax) === 0) return 0;
     return 100 / (parseInt(intMax) / parseInt(intValue));
