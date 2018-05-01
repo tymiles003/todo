fb780b6475027571509d9573dcdeef4b03d7eaeb
silksofthesoul <silksofthesoul@yandex.ru>
2018-05-02 02:40:45 +0300

    data encrypt (lol)

--- src/store/plugins.js
+++ src/store/plugins.js
@@ -1,7 +1,22 @@
 import {STORAGE_KEY} from './state';
-
+import {isFirstRun} from './state';
+import {keyGen} from './state';
+import {keySet} from './state';
+import {KEY} from './state';
+import crypt from '@/components/lib/crypt';
 export const localStoragePlugin = (store) => {
-  store.subscribe((mutation, items) => {
-    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
-  });
+  if (isFirstRun()){
+    let key = keyGen();
+    store.subscribe((mutation, items) => {
+      let data = crypt.crypt(JSON.stringify(items), key);
+      let dataEnd = keySet(data, key);
+      window.localStorage.setItem(STORAGE_KEY, dataEnd);
+    });
+  }else{
+    store.subscribe((mutation, items) => {
+      let data = crypt.crypt(JSON.stringify(items), KEY);
+      let dataEnd = keySet(data, KEY);
+      window.localStorage.setItem(STORAGE_KEY, dataEnd);
+    });
+  }
 };
--- src/store/state.js
+++ src/store/state.js
@@ -1,4 +1,11 @@
-export const STORAGE_KEY = 'todos-vuejs';
+import math from '@/components/lib/math';
+import crypt from '@/components/lib/crypt';
+import moment from 'moment';
+
+
+const KEY_LENGTH = 128;
+const KEY_HALF = KEY_LENGTH / 2;
+export const STORAGE_KEY = 'time-tracker';
 const TODO_KEY = 'todoList';
 const DEFAULT_OBJECT = {
   items: [],
@@ -15,7 +22,33 @@ const DEFAULT_OBJECT = {
   ]
 };
 const INIT_TEMPLATE = JSON.stringify(DEFAULT_OBJECT);
+let data = "";
 
-export const state = {
-  [TODO_KEY]: JSON.parse(window.localStorage.getItem(STORAGE_KEY)) !== null ? JSON.parse(window.localStorage.getItem(STORAGE_KEY))[TODO_KEY] : JSON.parse(INIT_TEMPLATE)
-};
+export function isFirstRun(){
+  return !(window.localStorage.getItem(STORAGE_KEY) !== null);
+}
+export function keyGen(){
+  return crypt.SHA512(String(math.uniqArrayInt(5,0,9).join(''))+String(parseInt(moment().format('x')))+window.navigator.appVersion+String(math.uniqArrayInt(5,0,9).join(''))).toString();
+}
+export function keySet(data, key){
+  return key.substr(0, KEY_HALF)+data+key.substr(KEY_HALF, KEY_LENGTH);
+}
+export function keyGet(data){
+  return {
+    key: data.substr(0,KEY_HALF)+data.substr(data.length - KEY_HALF,data.length),
+    data: data.substr(KEY_HALF, data.length - KEY_LENGTH)
+  };
+}
+let key = ""
+if (isFirstRun()) {
+  data = JSON.parse(INIT_TEMPLATE);
+} else {
+  let stor = window.localStorage.getItem(STORAGE_KEY);
+  key = keyGet(stor).key;
+  let exclData = keyGet(stor).data;
+  let decrypt = crypt.decrypt(exclData, key);
+  data = JSON.parse(decrypt)[TODO_KEY];
+}
+
+export const state = { [TODO_KEY]: data };
+export const KEY = key;
