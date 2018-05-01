dba4b32001beeb10bbd1dd62fd6434c7fdb221ad
silksofthesoul <silksofthesoul@yandex.ru>
2018-05-01 18:27:20 +0300

    YAmetrika

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue';
 import App from './App';
 import store from './store';
 import router from './router';
-
+import VueYandexMetrika from 'vue-yandex-metrika';
 // -----bootstrap:
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,6 +15,18 @@ import './assets/less/common.less';
 // import helper from '@/components/lib/todoHelpers';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueYandexMetrika, {
+  id: 48713990,
+  router: router,
+  env: process.env.NODE_ENV,
+  options: {
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: true
+  }
+});
 
 /* eslint-disable no-new */
 new Vue({
