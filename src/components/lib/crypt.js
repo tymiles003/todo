2fdd2abb9fb5ea1f266c06dbdb3de52b40c64864
Labuzov Evgeny <Evgeny.Labuzov@infotecs.ru>
2018-05-17 14:46:13 +0300

    чуть чуть там по копошились :-)

--- src/components/lib/crypt.js
+++ src/components/lib/crypt.js
@@ -1,5 +1,5 @@
 import CryptoJS from 'crypto-js';
-
+window.CryptoJS = CryptoJS;
 export default (() => {
   let obj = {};
 
@@ -7,12 +7,35 @@ export default (() => {
     return CryptoJS.SHA512(String(data));
   };
 
+  obj.wrap = (data) => {
+    let rawData = data;
+    let round_1 = CryptoJS.enc.Utf8.parse( rawData ).toString();
+    let round_2 = btoa(round_1);
+    let round_3 = CryptoJS.enc.Utf8.parse( round_2 ).toString();
+
+    return round_3;
+  }
+
+  obj.unwrap = (data) => {
+    let rawData = data;
+    let round_3 = CryptoJS.enc.Utf8.stringify(
+                CryptoJS.enc.Hex.parse(rawData)
+              );
+
+    let round_2 = atob(round_3);
+
+    let round_1 = CryptoJS.enc.Utf8.stringify(
+                  CryptoJS.enc.Hex.parse(round_2)
+                );
+    return round_1;
+  }
+
   obj.crypt = (data, key) => {
-    return CryptoJS.AES.encrypt(data, key).toString();
+    return obj.wrap(CryptoJS.AES.encrypt(data, key).toString());
   };
 
   obj.decrypt = (data, key) => {
-    let bytes = CryptoJS.AES.decrypt(data.toString(), key);
+    let bytes = CryptoJS.AES.decrypt(obj.unwrap(data.toString()), key);
     return bytes.toString(CryptoJS.enc.Utf8);
   };
 
