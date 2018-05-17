355c3eabcb22aa03682cfa12fdeccbf8aefbf62e
Labuzov Evgeny <Evgeny.Labuzov@infotecs.ru>
2018-05-17 11:31:59 +0300

    теперь сохраняеются данные пользователя на клиенте в зашифровоном виде, но приеэтом работает все с модулями.

--- src/store/UserData/plugins.js
+++ src/store/UserData/plugins.js
@@ -1,58 +1,29 @@
 import {STORAGE_KEY, isFirstRun, keyGen, keySet, KEY} from './state';
 import crypt from '@/components/lib/crypt';
 
-// export function localStoragePlugin (options = {}) {
-//   console.log('plugin test');
-//   return function (store) {
-//     const namespace = options.namespace || '';
-//     store.dispatch(namespace + 'pluginAction');
-//     console.log('plugin');
-//     if (isFirstRun()) {
-//       let key = keyGen();
-//       store.subscribe((mutation, items) => {
-//         let data = crypt.crypt(JSON.stringify(items), key);
-//         let dataEnd = keySet(data, key);
-//         window.localStorage.setItem(STORAGE_KEY, dataEnd);
-//       });
-//     } else {
-//       store.subscribe((mutation, items) => {
-//         let data = crypt.crypt(JSON.stringify(items), KEY);
-//         let dataEnd = keySet(data, KEY);
-//         window.localStorage.setItem(STORAGE_KEY, dataEnd);
-//       });
-//     }
-//   };
-// };
-
-export const localStoragePlugin = (store) => {
-  if (isFirstRun()) {
-    console.log('hello', store);
-    let key = keyGen();
-    store.subscribe((mutation, items) => {
-      console.log('kkkkkk');
-      let data = crypt.crypt(JSON.stringify(items), key);
-      let dataEnd = keySet(data, key);
-      window.localStorage.setItem(STORAGE_KEY, dataEnd);
-    });
-  } else {
-    store.subscribe((mutation, items) => {
-      let data = crypt.crypt(JSON.stringify(items), KEY);
-      let dataEnd = keySet(data, KEY);
-      window.localStorage.setItem(STORAGE_KEY, dataEnd);
-    });
-  }
-};
-
-export const myPlugin = (arg) => {
-  // called when the store is initialized
-  console.log('1', arg);
-  return store => {
-    console.log('2', arg);
-    store.subscribe((mutation, state) => {
-      console.log('3', arg);
-      console.log(state, 'eee');
-      // called after every mutation.
-      // The mutation comes in the format of `{ type, payload }`.
-    })();
+export const localStoragePlugin = (options) => {
+  const namespace = options.namespace;
+  return (store) => {
+    if (isFirstRun()) {
+      let key = keyGen();
+      store.subscribe((mutation, items) => {
+        console.log( options, namespace, items[namespace]);
+        if (namespace && items[namespace]) {
+          console.log(items);
+          let data = crypt.crypt(JSON.stringify(items[namespace]), key);
+          let dataEnd = keySet(data, key);
+          window.localStorage.setItem(STORAGE_KEY, dataEnd);
+        }
+      });
+    } else {
+      store.subscribe((mutation, items) => {
+        console.log( namespace, items[namespace]);
+        if (namespace && items[namespace]) {
+          let data = crypt.crypt(JSON.stringify(items[namespace]), KEY);
+          let dataEnd = keySet(data, KEY);
+          window.localStorage.setItem(STORAGE_KEY, dataEnd);
+        }
+      });
+    }
   };
 };
--- src/store/index.js
+++ src/store/index.js
@@ -4,35 +4,30 @@ import Vuex from 'vuex';
 import {UserDataState} from './UserData/state';
 import {UserDataMutations} from './UserData/mutations';
 import {UserDataGetters} from './UserData/getters';
-import {localStoragePlugin, myPlugin} from './UserData/plugins';
+import {localStoragePlugin} from './UserData/plugins';
 
 import {NotifyState} from './Notify/state';
 import {NotifyMutations} from './Notify/mutations';
 import {NotifyGetters} from './Notify/getters';
 
 Vue.use(Vuex);
-// let ooo = myPlugin('00');
 let UserData = {
   namespaced: true,
   state: UserDataState,
   mutations: UserDataMutations,
-  getters: UserDataGetters,
-  plugins: [
-    localStoragePlugin,
-    myPlugin,
-    store => store.dispatch('STORE_INIT')
-  ]
+  getters: UserDataGetters
 };
 let Notify = {
   namespaced: true,
   state: NotifyState,
   mutations: NotifyMutations,
-  getters: NotifyGetters,
+  getters: NotifyGetters
 };
 
 export default new Vuex.Store({
   modules: {
     UserData,
     Notify
-  }
+  },
+  plugins: [ localStoragePlugin({namespace: 'UserData'}) ]
 });
