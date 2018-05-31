1db2b31b66a1412e70879ef956183d40245c336e
Labuzov Evgeny <Evgeny.Labuzov@infotecs.ru>
2018-05-31 16:18:16 +0300

    методы для работы с массивами

--- src/library/array.js
+++ src/library/array.js
@@ -13,6 +13,33 @@ export default (() => {
     return false;
   };
 
+  obj.getItemInObjArrByKey = (key, value, array) => {
+    if (!key && value) return;
+    if (array.length === 0) return;
+    for (let i = 0; i <= array.length; ++i) {
+      if (!array[i]) return;
+      if (String(array[i][key]) === String(value)) {
+        return {
+          index: i,
+          object: array[i]
+        };
+      }
+    }
+    return false;
+  };
+
+  obj.deleteItemArrByElement = (element, array) => {
+    let keys = [];
+    array.forEach((item, i, arr) => {
+      if (item === element) {
+        keys.push(i);
+      }
+    });
+    keys.forEach((item, i, arr) => {
+      array.splice(item, item + 1);
+    });
+  };
+
   obj.equalArray = (arr, callback) => {
     for (var i = 0; i < arr.length; i++) {
       if (!callback(arr[i])) {
